7d1a78dd11242c4b07b60aa9cdb77d558aab4fab
Felipe Ayoub Zanini <fayoub94@hotmail.com>
2021-05-11 00:36:04 -0300

    Adicionado tratativa de exceções gerais

--- src/main/java/br/com/felipejoao/userapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/br/com/felipejoao/userapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -13,6 +13,7 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -27,6 +28,19 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     public ApiExceptionHandler(MessageSource messageSource) {
         this.messageSource = messageSource;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleNotMappedException(Exception ex, WebRequest web) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        Problem problem = Problem.builder()
+                .title(ex.getMessage())
+                .status(status.value())
+                .dateTime(OffsetDateTime.now())
+                .build();
+
+        return handleExceptionInternal(ex, problem, new HttpHeaders(), status, web);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
